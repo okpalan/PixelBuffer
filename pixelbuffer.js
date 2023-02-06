350386bb6965f3032ba31bdb847ce3c39e01382f
okpalan <89138089+okpalan@users.noreply.github.com>
2023-02-06 01:21:28 +0000

    Add files via upload

---
+++ pixelbuffer.js
@@ -0,0 +1,118 @@
+
+var PixelBuffer = function (width, height) {
+    this.width = width;
+    this.height = height;
+    this.data = new Uint8ClampedArray(width * height * 4);
+}
+
+PixelBuffer.prototype.getPixel = function (x, y) {
+    var index = (y * this.width + x) * 4;
+    return {
+        r: this.data[index],
+        g: this.data[index + 1],
+        b: this.data[index + 2],
+        a: this.data[index + 3]
+    }
+}
+
+PixelBuffer.prototype.setPixel = function (x, y, r, g, b, a) {
+    var index = (y * this.width + x) * 4;
+    this.data[index] = r;
+    this.data[index + 1] = g;
+    this.data[index + 2] = b;
+    this.data[index + 3] = a;
+}
+
+PixelBuffer.prototype.getData = function () {
+    return this.data;
+}
+
+PixelBuffer.prototype.getWidth = function () {
+    return this.width;
+}
+
+PixelBuffer.prototype.getHeight = function () {
+    return this.height;
+}
+
+PixelBuffer.prototype.setData = function (data) {
+    this.data = data;
+}
+PixelBuffer.prototype.setWidth = function (width) {
+    this.width = width;
+}
+
+PixelBuffer.prototype.setHeight = function (height) {
+    this.height = height;
+}
+
+PixelBuffer.prototype.clear = function (r, g, b, a) {
+    var data = this.data;
+    var len = data.length;
+    for (var i = 0; i < len; i += 4) {
+        data[i] = r;
+        data[i + 1] = g;
+        data[i + 2] = b;
+        data[i + 3] = a;
+    }
+}
+
+PixelBuffer.prototype.fill = function (r, g, b, a) {
+    var data = this.data;
+    var len = data.length;
+    for (var i = 0; i < len; i += 4) {
+        data[i] = r;
+        data[i + 1] = g;
+        data[i + 2] = b;
+        data[i + 3] = a;
+    }
+}
+
+PixelBuffer.prototype.drawPixelBuffer = function (pixelBuffer, x, y) {
+    var data = this.data;
+    var pixelBufferData = pixelBuffer.getData();
+    var pixelBufferWidth = pixelBuffer.getWidth();
+    var pixelBufferHeight = pixelBuffer.getHeight();
+    var len = pixelBufferData.length;
+    var index;
+    var pixelBufferIndex;
+    for (var i = 0; i < len; i += 4) {
+        index = ((y + Math.floor(i / 4 / pixelBufferWidth)) * this.width + x + (i / 4) % pixelBufferWidth) * 4;
+        pixelBufferIndex = i;
+        data[index] = pixelBufferData[pixelBufferIndex];
+        data[index + 1] = pixelBufferData[pixelBufferIndex + 1];
+        data[index + 2] = pixelBufferData[pixelBufferIndex + 2];
+        data[index + 3] = pixelBufferData[pixelBufferIndex + 3];
+    }
+}
+
+PixelBuffer.prototype.getDataURL = function () {
+    var canvas = document.createElement('canvas');
+    var context = canvas.getContext('2d');
+    canvas.width = this.width;
+    canvas.height = this.height;
+    var imageData = context.createImageData(this.width, this.height);
+    imageData.data.set(this.data);
+    context.putImageData(imageData, 0, 0);
+    return canvas.toDataURL();
+}
+
+PixelBuffer.prototype.drawPixel = function (x, y, r, g, b, a) {
+    var index = (y * this.width + x) * 4;
+    this.data[index] = r;
+    this.data[index + 1] = g;
+    this.data[index + 2] = b;
+    this.data[index + 3] = a;
+}
+
+PixelBuffer.prototype.getData = function () {
+    return this.data;
+}
+// usage the pixel buffer
+// var pixelBuffer = new PixelBuffer(800, 600);
+// var imageData = context.createImageData(800, 600);
+// imageData.data.set(pixelBuffer.getData());
+// context.putImageData(imageData, 0, 0);
+
+
+
